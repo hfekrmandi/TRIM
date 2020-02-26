d43c34e7b6310e7c579db59622fc08294d571df5
MohamedNaveed <mohdnaveed96@gmail.com>
2020-02-26 12:19:48 -0600

    scaling of agents working

--- code/Compare_with_Full_Filter/choose_sensors.m
+++ code/Compare_with_Full_Filter/choose_sensors.m
@@ -1,15 +1,12 @@
 function [C,ind_sensor] = choose_sensors(Ct,nSensor)
+% This function chooses nSensor rows from Ct that have the highest sum
+% of absolute value. The logic is that we want to choose the lines that 
+% provide meaningful observation matrix.
 norm_c = zeros(size(Ct,1),1);
 for i = 1 : size(Ct,1)
     norm_c(i) = sum(abs(Ct(i,:)));
 end
 
 [B,ind] = sort(norm_c,'descend');
-% eig(Ct(ind(1:10),:)*At)
-% 
-% G = ss(At,Bt,Ct(ind(1:1),:),[],1)
-% Gc = canon(G,'modal');
-% Gc.A
-% Gc.C
 ind_sensor = ind(1:nSensor);
 C = Ct(ind(1:nSensor),:);
